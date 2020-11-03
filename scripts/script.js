b1247c94dd023b8feaccb687931049d33b648a57
Beast <47059999+Im-Beast@users.noreply.github.com>
2020-11-03 22:44:30 +0100

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -103,8 +103,8 @@ function draw() {
             let curDist = Math.sqrt(Math.abs(distX)*Math.abs(distY));
             
             star.velocity = [
-                deltaX/curDist*star.speed*25,
-                deltaY/curDist*star.speed*25
+                deltaX/curDist*star.speed*distX,
+                deltaY/curDist*star.speed*distY
             ];
 
             context.beginPath();
